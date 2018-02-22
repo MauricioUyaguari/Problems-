f79ff6d0d51c38a3e7d184e8907354f1109e6aa2
Mauricio Uyaguari <lmauricio12@gmail.com>
2018-02-22 17:15:47 -0500

    search range

--- lib/leetcode_31_35.py
+++ lib/leetcode_31_35.py
@@ -30,3 +30,31 @@ def search_range(nums, target):
     if not start and not finish:
         return [-1, -1]
     return [start, finish]
+
+nums = [1,2,5,5,5,9]
+target = 5
+
+def search_range(nums, target):
+    left_index = extreme_insertion_index(nums, target, True)
+    if left_index == len(nums) or nums[left_index] != target:
+        return [-1, -1]
+    return [left_index, extreme_insertion_index(nums,target, False)-1]
+
+
+def extreme_insertion_index(nums, target, left):
+    low = 0
+    high = len(nums)
+    print nums
+    print "low, high"
+    print low, high
+    while low < high:
+        mid = (low + high)//2
+        print "low, high, mid, nums[mid]"
+        print low,high, mid, nums[mid]
+        if nums[mid] > target or (left and target == nums[mid]):
+            high = mid
+        else:
+            low = mid + 1
+    return low
+
+search_range(nums, target)
